50ccb89be6b3826185574bad71d4f7cee4efbd2d
류지승 <8514199@gmail.com>
2023-09-19 17:56:17 +0900

    init

--- myenv/app.py
+++ myenv/app.py
@@ -1,8 +1,10 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 from dotenv import load_dotenv
-from quizGenrator import quizGenerator
+from chatbot import chat, update
+from pymongo import MongoClient
 
-# .env 파일 로드
+
+
 load_dotenv()
 import os
 
@@ -10,20 +12,46 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    database_uri = os.environ.get('DATABASE_URI')
-    print(database_uri)
+   
 
     return 'Hello, World!'
 
-@app.route("/quiz")
-def quiz():
-    result = quizGenerator("rest api 가 뭔가요?")
-    return result
+
     
 
 @app.route("/health")
 def health_check():
     return 'OK'
+
+@app.route('/text-to-ai', methods=['POST'])
+def text_to_ai():
+    try:
+        # POST 요청에서 JSON 데이터 가져오기
+        data = request.get_json()
+
+        text = data.get('text')
+        
+        update(text)
+
+
+        return jsonify({'message': 'Data received and stored successfully'}), 201
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+    
+@app.route('/chat', methods=['POST'])
+def chat_to_ai():
+    try:
+        # POST 요청에서 JSON 데이터 가져오기
+        data = request.get_json()
+
+        # JSON 데이터 처리ß
+        text = data.get('text')
+        
+        response = chat(text)
+
+        return jsonify({'message': response}), 201
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
     app.run()
